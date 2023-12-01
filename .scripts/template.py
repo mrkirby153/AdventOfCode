4f223722de2363092fda2cee2a20298494f97cfd
mrkirby153 <mr.austinwhyte@gmail.com>
2023-11-30 22:49:03 -0800

    [msc] Allow running individual parts

--- .scripts/template.py
+++ .scripts/template.py
@@ -1,15 +1,17 @@
-from aoc_common import get_puzzle_input, sprint
+from aoc_common import get_puzzle_input, run, sprint
 from aoc_common.benchmark import print_timings
 
 input_data = get_puzzle_input()
+
 
 @print_timings
 def part_1():
     pass
 
+
 @print_timings
 def part_2():
     pass
 
-print(f"Part 1: {part_1()}")
-print(f"Part 2: {part_2()}")
+
+run(part_1, part_2)
--- common/aoc_common/__init__.py
+++ common/aoc_common/__init__.py
@@ -2,9 +2,18 @@ import argparse
 from functools import cache
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--sample', '-s', help='Run with sample data', action='store_true', default=False)
-parser.add_argument('--file', '-f', help='Run against a specific input file')
+parser.add_argument(
+    "--sample", "-s", help="Run with sample data", action="store_true", default=False
+)
+parser.add_argument("--file", "-f", help="Run against a specific input file")
+parser.add_argument(
+    "--one", "-1", help="Run only part 1", action="store_true", default=False
+)
+parser.add_argument(
+    "--two", "-2", help="Run only part 2", action="store_true", default=False
+)
 parsed_args = parser.parse_args()
+
 
 def sprint(*args, **kwargs):
     """
@@ -13,11 +22,22 @@ def sprint(*args, **kwargs):
     if parsed_args.sample:
         print(*args, **kwargs)
 
+
 @cache
 def get_puzzle_input():
     if parsed_args.file:
         file_name = parsed_args.file
     else:
-        file_name = 'input.txt' if not parsed_args.sample else 'sample.txt'
+        file_name = "input.txt" if not parsed_args.sample else "sample.txt"
     with open(file_name) as f:
-        return [x.replace('\n', '') for x in f.readlines()]
+        return [x.replace("\n", "") for x in f.readlines()]
+
+
+def run(part_1, part_2):
+    if not parsed_args.one and not parsed_args.two:
+        print(f"Part 1: {part_1()}")
+        print(f"Part 2: {part_2()}")
+    elif parsed_args.one:
+        print(f"Part 1: {part_1()}")
+    elif parsed_args.two:
+        print(f"Part 2: {part_2()}")
