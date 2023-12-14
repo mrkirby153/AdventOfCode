76adb2d66aa94a54b29bb4e0fa7c4275f6323e26
mrkirby153 <mr.austinwhyte@gmail.com>
2023-12-13 21:57:34 -0800

    [2023] Day 14 pt 2 bruteforce

--- 2023/day14/day14.py
+++ 2023/day14/day14.py
@@ -1,6 +1,7 @@
 from aoc_common import get_puzzle_input, run, sprint
 from aoc_common.benchmark import print_timings
 from aoc_common.io import as_complex_matrix_dict, to_2d_matrix, print_matrix_dict
+from tqdm import tqdm
 
 input_data = get_puzzle_input()
 
@@ -11,10 +12,8 @@ def load_input(data):
     return {k: v for k, v in matrix.items() if v != "."}
 
 
-def move_grid_north(grid):
+def move_grid_north(grid, max_x, max_y):
     new_grid = {}
-    max_y = max([int(x.imag) for x in grid.keys()])
-    max_x = max([int(x.real) for x in grid.keys()])
     absent = object()  # Sentinel for not in grid
 
     moved = False
@@ -43,10 +42,103 @@ def move_grid_north(grid):
     return new_grid, moved
 
 
-def shift(grid):
-    grid, moved = move_grid_north(grid)
+def move_grid_south(grid, max_x, max_y):
+    new_grid = {}
+    absent = object()  # Sentinel for not in grid
+
+    moved = False
+    for y in range(max_y, -1, -1):
+        for x in range(0, max_x + 1):
+            point = complex(x, y)
+            val = grid.get(point, absent)
+            if val == absent:
+                continue  # Skip this point
+
+            if val == "#":
+                new_grid[point] = val
+                continue  # Fixed rocks don't move
+
+            new_point = point + 1j
+            if new_point.imag > max_y:
+                new_point = point  # Point is at the bottom
+
+            if grid.get(new_point, absent) in ["#", "O"]:
+                new_grid[point] = val  # Can't move
+            else:
+                new_grid[new_point] = val
+                moved = True
+
+    # print_matrix_dict(new_grid)
+    return new_grid, moved
+
+
+def move_grid_east(grid, max_x, max_y):
+    new_grid = {}
+    absent = object()  # Sentinel for not in grid
+
+    moved = False
+    for x in range(0, max_x + 1):
+        for y in range(0, max_y + 1):
+            point = complex(x, y)
+            val = grid.get(point, absent)
+            if val == absent:
+                continue
+            if val == "#":
+                new_grid[point] = val
+                continue
+
+            new_point = point + complex(1, 0)
+            if new_point.real > max_x:
+                new_point = point
+
+            if grid.get(new_point, absent) in ["#", "O"]:
+                new_grid[point] = val
+            else:
+                new_grid[new_point] = val
+                moved = True
+    return new_grid, moved
+
+
+def move_grid_west(grid, max_x, max_y):
+    new_grid = {}
+    absent = object()  # Sentinel for not in grid
+
+    moved = False
+    for x in range(max_x, -1, -1):
+        for y in range(0, max_y + 1):
+            point = complex(x, y)
+            val = grid.get(point, absent)
+            if val == absent:
+                continue
+            if val == "#":
+                new_grid[point] = val
+                continue
+
+            new_point = point - complex(1, 0)
+            if new_point.real < 0:
+                new_point = point
+
+            if grid.get(new_point, absent) in ["#", "O"]:
+                new_grid[point] = val
+            else:
+                new_grid[new_point] = val
+                moved = True
+    return new_grid, moved
+
+
+def perform_spin(grid, max_x, max_y):
+    for func in [move_grid_north, move_grid_west, move_grid_south, move_grid_east]:
+        grid = shift(grid, func, max_x, max_y)
+    return grid
+
+
+def shift(grid, direction, max_x, max_y):
+    orig_grid = grid
+    grid, moved = direction(grid, max_x, max_y)
     while moved:
-        grid, moved = move_grid_north(grid)
+        grid, moved = direction(grid, max_x, max_y)
+
+    assert len(grid) == len(orig_grid), "lost something"
     return grid
 
 
@@ -61,8 +153,7 @@ def print_matrix_dict(
     for y in range(min_y, max_y + 1):
         row_str = ""
         for x in range(min_x, max_x + 1):
-            row_str += matrix.get(complex(x, y), " ")
-        row_str += f" {y}"
+            row_str += matrix.get(complex(x, y), ".")
         print_func(row_str)
 
 
@@ -80,7 +171,7 @@ def part_1():
     print_matrix_dict(grid, min_x, max_x, min_y, max_y)
     sprint("-----")
     # move_grid_north(grid)
-    grid = shift(grid)
+    grid = shift(grid, move_grid_north, max_x, max_y)
     print_matrix_dict(grid, min_x, max_x, min_y, max_y)
 
     load = 0
@@ -93,7 +184,26 @@ def part_1():
 
 @print_timings
 def part_2():
-    pass
+    grid = load_input(input_data)
+
+    min_x, max_x = 0, max([int(x.real) for x in grid.keys()])
+    min_y, max_y = 0, max([int(x.imag) for x in grid.keys()])
+
+    print_matrix_dict(grid, min_x, max_x, min_y, max_y)
+
+    for i in tqdm(range(1000000000)):
+        # sprint("Cycle", i)
+        for func in [move_grid_north, move_grid_west, move_grid_south, move_grid_east]:
+            # sprint(func)
+            grid = shift(grid, func, max_x, max_y)
+            # print_matrix_dict(grid, min_x, max_x, min_y, max_y)
+
+    load = 0
+
+    for point, val in grid.items():
+        if val == "O":
+            load += max_y + 1 - point.imag
+    return int(load)
 
 
 run(part_1, part_2, __name__)
