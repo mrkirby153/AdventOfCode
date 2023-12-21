bfd65e90f8c5211cf0ef2de230ceae4d0fb0d7fb
mrkirby153 <mr.austinwhyte@gmail.com>
2023-12-20 23:46:53 -0800

    [2023] Day 21 pt 2

--- 2023/day21/day21.py
+++ 2023/day21/day21.py
@@ -1,7 +1,13 @@
 from aoc_common import get_puzzle_input, run, sprint
 from aoc_common.benchmark import print_timings
-from aoc_common.io import as_complex_matrix_dict, to_2d_matrix, print_matrix_dict
+from aoc_common.io import (
+    as_complex_matrix_dict,
+    to_2d_matrix,
+    print_matrix_dict,
+    get_grid_dimensions,
+)
 from aoc_common.plane import neighbors
+from tqdm import tqdm
 
 input_data = get_puzzle_input()
 
@@ -10,12 +16,12 @@ def load_input(input_data):
     return as_complex_matrix_dict(to_2d_matrix(input_data))
 
 
-def take_step(garden_map, points):
+def take_step(garden_map, points, x_dim, y_dim):
     reachable_points = set()
 
     for point in points:
         for neighbor in neighbors(point):
-            if garden_map.get(neighbor) in ".S":
+            if garden_map[map_points(neighbor, x_dim, y_dim)] in ".S":
                 reachable_points.add(neighbor)
     return reachable_points
 
@@ -24,6 +30,16 @@ def find_start(garden_map):
     for point, value in garden_map.items():
         if value == "S":
             return point
+
+
+def map_points(point, x_dim, y_dim):
+    (min_x, max_x), (min_y, max_y) = x_dim, y_dim
+
+    # The grid repeats indefinitely, so we need to map the point to the
+    # smallest positive value
+    x = point.real % (max_x - min_x + 1)
+    y = point.imag % (max_y - min_y + 1)
+    return complex(x, y)
 
 
 @print_timings
@@ -35,10 +51,12 @@ def part_1():
     start = find_start(garden_map)
     assert start is not None, "No start found"
 
+    x_dim, y_dim = get_grid_dimensions(garden_map)
+
     points = {start}
 
     for _ in range(64):
-        points = take_step(garden_map, points)
+        points = take_step(garden_map, points, x_dim, y_dim)
 
     def mapper(point, val):
         if point in points:
@@ -54,9 +72,32 @@ def part_1():
     return len(points)
 
 
+def interpolate(poly, n):
+    b0 = poly[0]
+    b1 = poly[1] - poly[0]
+    b2 = poly[2] - poly[1]
+    return b0 + b1 * n + (n * (n - 1) // 2) * (b2 - b1)
+
+
 @print_timings
 def part_2():
-    pass
+    garden_map = load_input(input_data)
+
+    start = find_start(garden_map)
+    assert start is not None, "No start found"
+
+    x_dim, y_dim = get_grid_dimensions(garden_map)
+
+    width = x_dim[1] - x_dim[0] + 1
+    steps = 26501365
+    accumulated = []
+
+    for step in [65, 65 + 131, 65 + 131 * 2]:
+        points = {start}
+        for _ in tqdm(range(step)):
+            points = take_step(garden_map, points, x_dim, y_dim)
+        accumulated.append(len(points))
+    return interpolate(accumulated, steps // width)
 
 
 run(part_1, part_2, __name__)
